995fe3ac6f2703ab21c68672527d9af062ae80ff
arreese <arielle.reese@kinandcarta.com>
2021-03-01 12:26:51 -0600

    Created security config to fix security error for POST

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/UserConfig.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfulwebservices.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+@Configuration
+@PropertySource("classpath:application.properties")
+public class UserConfig {
+    @Value("${spring.security.user.name}")
+    private String userName;
+    @Value("${spring.security.user.password}")
+    private String password;
+    public String getUserName() {
+        return userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
